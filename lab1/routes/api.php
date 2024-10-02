e0e1c1b8589a89140d36bc3aa684a092cb351b2e
Yaroslav <kogutyaroslav3947@gmail.com>
2024-10-01 21:41:23 -0500

    fix(user): fix route issue in api and user model

--- lab1/routes/api.php
+++ lab1/routes/api.php
@@ -1,13 +1,11 @@
 <?php
-
+  
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-
+  
+use App\Http\Controllers\API\RegisterController;
+use App\Http\Controllers\API\ProductController;
+   
 Route::controller(RegisterController::class)->group(function(){
     Route::post('register', 'register');
     Route::post('login', 'login');
